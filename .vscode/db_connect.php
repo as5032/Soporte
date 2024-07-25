951d2d5353c92d7075b52b9eff5c1468d058ceb6
as5032 <alejandrorosas61@gmail.com>
2024-07-24 18:57:42 -0600

    cambio de contraseña

--- .vscode/db_connect.php
+++ .vscode/db_connect.php
@@ -2,7 +2,7 @@
 
   $servername = "localhost";
   $username = "root";
-  $password = "4dm1n1str4ad0r";
+  $password = "4dm1n1str4d0r";
   $dbname = "css_db";
 
   $conn= new mysqli($servername,$username,$password,$dbname)or die("Could not connect to mysql".mysqli_error($con));
